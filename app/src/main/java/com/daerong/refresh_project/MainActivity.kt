09feb7d1b92ee35dff4af8e82c3206f6569f990f
hyerong <60914650+hye-rong@users.noreply.github.com>
2021-02-14 18:07:32 +0900

    mainActivity
    mainActivity&bottomnavi

--- app/src/main/java/com/daerong/refresh_project/MainActivity.kt
+++ app/src/main/java/com/daerong/refresh_project/MainActivity.kt
@@ -2,10 +2,14 @@ package com.daerong.refresh_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupWithNavController(main_bottom_navigation, findNavController(R.id.nav_host))
     }
 }
