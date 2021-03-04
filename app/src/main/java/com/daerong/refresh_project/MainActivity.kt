c5e3be4f5ac26babade360b9c4aa37ac22fd6c8b
imhynni <68845258+imhynni@users.noreply.github.com>
2021-03-04 18:27:01 +0900

    test

--- app/src/main/java/com/daerong/refresh_project/MainActivity.kt
+++ app/src/main/java/com/daerong/refresh_project/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         NavigationUI.setupWithNavController(main_bottom_navigation, findNavController(R.id.nav_host))
+    //
     }
 }
